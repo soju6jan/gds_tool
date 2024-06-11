9a422752279bd17d2ad461a79f2604c9c042d08f
flaskfarm <flaskfarm@gmail.com>
2024-06-12 00:39:47 +0900

    - 1.1.9 (2024.06.11)   GDS 변경사항 - 방송하기 추가.   PLEX MATE 4단계 구드 이미지 호스팅 코드 암호화로 추가.

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ import os
 from support import SupportSC
 
 try:
-    if os.path.exists(os.path.join(os.path.dirname(__file__), '1gdrive.py')):
+    if os.path.exists(os.path.join(os.path.dirname(__file__), 'gdrive.py')):
         from .gdrive import SSGDrive
     else:
         SSGDrive = SupportSC.load_module_f(__file__, 'gdrive').SSGDrive
